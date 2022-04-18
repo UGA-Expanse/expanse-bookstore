79464ec17598140f8a416faa2b0eeb45615b6a2c
niknab <nabuga@uga.edu>
2022-04-17 22:27:59 -0400

    Added Catalog_item to capture categories

---
+++ src/main/java/rocks/j5/uga/expanse/model/CategoryItem.java
@@ -0,0 +1,45 @@
+package rocks.j5.uga.expanse.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "category_item")
+public class CategoryItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+}
--- src/main/resources/db/migration/V0016__Create_Collection_Table.sql
+++ src/main/resources/db/migration/V0016__Create_Collection_Table.sql
@@ -1,8 +1,9 @@
 CREATE TABLE category_item (
-                               id INT auto_increment,
-                               book_id INT,
-                               category_id INT,
-                               CONSTRAINT pk_cat_item PRIMARY KEY (id),
-                               CONSTRAINT fk_cat_catitem FOREIGN KEY (category_id) REFERENCES category (id),
-                               CONSTRAINT fk_book_catitem FOREIGN KEY (book_id) REFERENCES book (book_id)
+                                id INT auto_increment,
+                                book_id INT,
+                                category_id INT,
+                                CONSTRAINT pk_cat_item PRIMARY KEY (id),
+                                CONSTRAINT fk_cat_catitem FOREIGN KEY (category_id) REFERENCES category (id),
+                                CONSTRAINT fk_book_catitem FOREIGN KEY (book_id) REFERENCES book (book_id),
+                                CONSTRAINT cat_item_unique UNIQUE (book_id,category_id)
 );
